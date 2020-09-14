cf68e2c8ceeee12d9c154ad2e9b33ca5baadd254
t.obata <ec2-user@ip-172-31-33-234.ec2.internal>
2020-09-14 04:48:01 +0000

    model、controller、viewファイル作成、ルーティング作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    
+  def show
+  
+  end
+
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+  
+  end
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+root to: "posts#index"
+
+get "login", to: "sessions#new"
+  #/loginというURLをGETすると、sessions_controllerのnewアクションを実行
+post "login", to: "sessions#create"
+  #loginページでpostすると～
+delete "logout", to: "sessions#destroy"
+  #logoutとしてdeleteをリクエストすると～
+
+get "signup", to: "users#new"
+ #ユーザ登録画面
+ 
+resources :posts
+# RESTfulなルーティング
+  
+resources :user, only:[:show, :new, :create, :edit]
+# は、下記をまとめて記述したもの。
+# get "user/:id" to: "users#show"
+# get "user" to: "users#neww"
+# post "user" to: "users#create"
+# get "user/:id/edit" to: "users#edit"
 end
